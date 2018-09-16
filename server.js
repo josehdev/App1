35428e622ff8ed7164ac06d99a425c30f222bac2
josehdev <joseh.75@gmail.com>
2018-09-15 22:14:44 -0400

    added 456

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function (req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello JH Super Code123 and Azure Web Apps!</h1>');
+    res.end('<h1>Hello JH Super Code123456 and Azure Web Apps!</h1>');
 }).listen(process.env.PORT);
